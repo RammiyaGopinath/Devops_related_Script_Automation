91f29bd7845c77b5fb8644225a22d2f3339213b2
RammiyaGopinath <rammiya.baskar@cognizant.com>
2021-01-27 13:15:02 +0530

    Initial commit

---
+++ app/src/main/java/com/example/devops_script_automation/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.devops_script_automation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
