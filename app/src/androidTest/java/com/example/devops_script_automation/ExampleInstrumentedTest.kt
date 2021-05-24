a81cc29f23387cadab8d7ed6ec44d2c16a8c2bc9
RammiyaGopinath <rammiya.baskar@cognizant.com>
2021-05-24 20:45:26 +0530

    services

--- app/src/androidTest/java/com/example/devops_script_automation/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/devops_script_automation/ExampleInstrumentedTest.kt
@@ -1,5 +1,6 @@
 package com.example.devops_script_automation
 
+import androidx.test.core.app.ActivityScenario
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
 
@@ -7,6 +8,9 @@ import org.junit.Test
 import org.junit.runner.RunWith
 
 import org.junit.Assert.*
+import org.junit.Before
+import tools.fastlane.screengrab.Screengrab
+import tools.fastlane.screengrab.UiAutomatorScreenshotStrategy
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -20,5 +24,18 @@ class ExampleInstrumentedTest {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
         assertEquals("com.example.devops_script_automation", appContext.packageName)
+    }
+
+    @Before
+    fun setUp() {
+        ActivityScenario.launch(MainActivity::class.java)
+        //Screengrab.setDefaultScreenshotStrategry(UiAutomatorScreenshotStrategy())
+    }
+
+    @Test
+    fun captureScreen() {
+        // Delay 500 millis for app launch to main screen
+        Thread.sleep(500)
+        Screengrab.screenshot("main_screen")
     }
 }
